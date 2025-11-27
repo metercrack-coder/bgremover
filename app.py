f08d8fb4cb26e73217f9c61df4684d26a42b1d23
metercrack-coder <metercrack@gmail.com>
2025-11-27 15:58:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -33,5 +33,9 @@ def remove_bg():
 def home():
     return "API is running!"
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+    
